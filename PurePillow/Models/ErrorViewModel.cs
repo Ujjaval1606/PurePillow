1075c6bdcbe9ce39af0c537291c35e8bf9602a63
W0789986 <W0789986@ADL-A2303A-08>
2023-10-13 16:32:48 -0400

    Add project files.

---
+++ PurePillow/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PurePillow.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
